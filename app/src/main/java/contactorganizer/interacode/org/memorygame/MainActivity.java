49fe0b020bd22975657d57aff3a57646b0172176
yoadayache <yoad.ayache@gmail.com>
2015-06-27 13:03:25 +0300

    yoad up

--- app/src/main/java/contactorganizer/interacode/org/memorygame/MainActivity.java
+++ app/src/main/java/contactorganizer/interacode/org/memorygame/MainActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    Button btn_image1;
+    Button btn_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_image1 = (Button)findViewById(R.id.image1);
+        btn_image1.setOnClickListener(this);
     }
 
 
@@ -42,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
 
 
         boolean isPressed = false;
-        R.id.image1.setOnClickListener(buttonListener);
+        btn_image1.setOnClickListener(this);
 
         OnClickListener buttonListener = new OnClickListener() {
             @Override
